18761a94d5748c9c652dc2ce2f3edd41d8f331ad
Brayden <brayden@roastlog.com>
2016-06-01 11:52:33 -0600

    temp migration fix

--- provider/oauth2/admin.py
+++ provider/oauth2/admin.py
@@ -16,7 +16,4 @@ class ClientAdmin(admin.ModelAdmin):
     list_display = ('url', 'user', 'redirect_uri', 'client_id', 'client_type')
     raw_id_fields = ('user',)
 
-admin.site.register(AccessToken, AccessTokenAdmin)
-admin.site.register(Grant, GrantAdmin)
-admin.site.register(Client, ClientAdmin)
-admin.site.register(RefreshToken)
+
